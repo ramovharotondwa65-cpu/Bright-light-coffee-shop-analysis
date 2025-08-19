9231b3742c344e6c5995f445a68e9335bf2fa97c
Rotondwa Ramovha <ramovharotondwa65@gmail.com>
2025-08-19 21:35:58 +0200

    draft one for analysis for Bright_light_coffee_shop  in SQL

---
+++ bright_light_coffee_script.sql
@@ -0,0 +1,55 @@
+--to view the full table
+select * from 
+bright_coffee.sales.transactions
+limit 10;
+
+-- anaylsis of the table
+select 
+
+to_date(transaction_date)as date_name,
+dayname(to_date(transaction_date)) as day_name,
+monthname(to_date(transaction_date)) as month_name,
+to_char(to_date(transaction_date),'YYYYMM') as month_id,
+
+count(distinct TRANSACTION_ID) as total_transactions,
+count(distinct STORE_ID) as total_Stores,
+count(distinct PRODUCT_ID) as total_prodcts,
+
+transaction_time,
+case
+when transaction_time between '06:00:00' AND '12:59:59' THEN 'morning'
+when transaction_time between '13:00:00' AND '16:059:59' THEN 'afternoon'
+when transaction_time between  '17:00:00' AND '20:59:59'  THEN 'Evening'
+END as time_buckets
+ from 
+bright_coffee.sales.transactions
+group by all;
+
+--finding the mini value of the transaction_time
+select min(TRANSACTION_TIME)
+from bright_coffee.sales.transactions;
+
+--finding the max value of the transaction_time
+select max(TRANSACTION_TIME)
+from 
+bright_coffee.sales.transactions;
+
+--placing transaction_time in a bucket
+select
+transaction_time,
+
+case
+when transaction_time between '06:00' AND '13:00' THEN 'morning'
+when transaction_time between '13:01' AND '17:00' THEN 'afternoon'
+when transaction_time between  '17:01' AND '20:59'  THEN 'Evening'
+END as time_buckets
+ from 
+bright_coffee.sales.transactions;
+
+
+
+
+
+
+
+
